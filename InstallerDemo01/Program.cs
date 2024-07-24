9953750205057616e1d8f3f79af63c029c73473e
AyushKumar2395 <ayush@ctsglobalconnect.com>
2024-07-24 17:55:24 +0530

    Add project files.

---
+++ InstallerDemo01/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = string.Empty;
+bool isReset = true;
+char userInp = '\0';
+while (isReset)
+{
+    try
+    {
+        if (userInp == '\0')
+        {
+            Console.WriteLine("=========Enter any value======");
+            name = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("=========You entered========");
+            Console.WriteLine(name);
+        }
+        Console.WriteLine("========Want to try again, press (y for yes / n for no)======");
+        userInp = Convert.ToChar(Console.ReadLine() ?? string.Empty);
+        if (userInp == 'y')
+        {
+            userInp = '\0';
+            continue;
+        }
+        else if (userInp == 'n')
+        {
+            isReset = false;
+            Console.WriteLine("=======Thanks for using our application========");
+            Console.ReadKey();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        continue;
+    }
+}
